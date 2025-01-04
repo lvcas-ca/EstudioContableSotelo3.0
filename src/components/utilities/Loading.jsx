e9efa7ef946167fd16342b34f72b8009e3059fa9
lvcas-ca <lucas_.caceres@hotmail.com.ar>
2025-01-04 01:04:50 -0300

    test loading

---
+++ src/components/utilities/Loading.jsx
@@ -0,0 +1,9 @@
+
+
+export const Loading = () => {
+return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <h2 className="text-customBlack">🌀 Loading...</h2>
+    </div>
+)
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,43 @@
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
-
-import '../Home.css';
-import QuienesSomos from '../components/QuienesSomos/QuienesSomos';
-import { Header } from '../components/Header';
-import { Servicios } from '../components/Servicios/Servicios';
-import { Contacto } from '../components/Contacto/Contacto';
-import { Footer } from '../components/Footer/Footer';
+import { Suspense } from "react";
+import "../Home.css";
+import QuienesSomos from "../components/QuienesSomos/QuienesSomos";
+import { Header } from "../components/Header";
+import { Servicios } from "../components/Servicios/Servicios";
+import { Contacto } from "../components/Contacto/Contacto";
+import { Footer } from "../components/Footer/Footer";
 import MarcasQueTrabajanConNosotros from "../components/MarcasQueTrabajanConNosotros/MarcasQueTrabajanConNosotros";
+import { Loading } from "../components/utilities/Loading";
 
-
-
-function Home () {
-    return (
-        <>
-        <header >
-            <Header/>
+function Home() {
+  return (
+    <>
+      <Suspense fallback={<Loading/>}>
+        <header>
+          <Header />
         </header>
-        <main >
-           <SpeedInsights /> {/* Aquí se renderiza el componente para medir el rendimiento */}
-           <Analytics />
-           <div className='px-[10px] md:px-[150px]'>
-             <Servicios/> 
-           </div>
-           
-             <Contacto/> 
 
-              <div className='px-[10px] md:px-[150px] bg-[#1C1D20]'>
-                 <QuienesSomos/>
-                 <MarcasQueTrabajanConNosotros/>   
-              </div>
-             
-           
-          
+        <main>
+          <SpeedInsights />{" "}
+          {/* Aquí se renderiza el componente para medir el rendimiento */}
+          <Analytics />
+          <div className="px-[10px] md:px-[150px]">
+            <Servicios />
+          </div>
+          <Contacto />
+          <div className="px-[10px] md:px-[150px] bg-[#1C1D20]">
+            <QuienesSomos />
+            <MarcasQueTrabajanConNosotros />
+          </div>
         </main>
-        <div className='px-[10px] md:px-[30px] lg:px-[150px] bg-bgColorFooter'>
-          <Footer/>
+        <div className="px-[10px] md:px-[30px] lg:px-[150px] bg-bgColorFooter">
+          <Footer />
         </div>
-      
-          
-        </>
-      )
+      </Suspense>
+    </>
+  );
 }
 
 export default Home;
